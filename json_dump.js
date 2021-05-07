1f8759eea8bcad403d6518f7838ee7635e306d4b
Rameezk3 <39254518+Rameezk3@users.noreply.github.com>
2021-05-07 12:55:39 +0200

    Add files via upload

---
+++ json_dump.js
@@ -0,0 +1,143 @@
+{'analysis_1': <bound method NDFrame.to_json of 0        bot
+ 1    desktop
+ 2      other
+ 3      phone
+ 4     tablet
+ Name: 0, dtype: object>,
+ 'analysis_2': <bound method NDFrame.to_json of                               datetime  \
+ 3058  2021-01-23 11:19:09.873000+00:00   
+ 3068  2021-01-23 12:03:21.649000+00:00   
+ 39959 2021-01-23 11:00:01.442999+00:00   
+ 41847 2021-01-23 11:00:01.350000+00:00   
+ 71446 2021-01-23 11:00:00.737000+00:00   
+ 
+                                      session_id  \
+ 3058   92f39c0081e995777f2e3ce857f1367703a1e63f   
+ 3068   ded6d0a982266a91d4b17fc490425be950a7193b   
+ 39959  d500518cc68fbd0960fc802aca9dd2a92593c9ea   
+ 41847  2475f7655ea3adadc5cf0e4b799ac43e11d8c8c7   
+ 71446  a3fef7660ebc7dbd05a55bbe033325eb2125fd17   
+ 
+                                      visitor_id  is_internal_ip  \
+ 3058   92f39c0081e995777f2e3ce857f1367703a1e63f           False   
+ 3068   ded6d0a982266a91d4b17fc490425be950a7193b           False   
+ 39959  6319db4a62abaca90ad7692d26bf7257dac66c08           False   
+ 41847  2475f7655ea3adadc5cf0e4b799ac43e11d8c8c7           False   
+ 71446  f37a3a2285c601739a95c5e569514c4508423b00           False   
+ 
+             page_type                       event_type  \
+ 3058   apartment_view                   page_view_init   
+ 3068              cms                        page_view   
+ 39959     search_page  search_apartment-preview-closed   
+ 41847  apartment_view                   page_view_init   
+ 71446     search_page            Click on filter label   
+ 
+                                                   params     om_source  \
+ 3058   {"page":{"page_type":"apartment_view","country...  no_om_source   
+ 3068     {"event_content":{},"page":{"page_type":"cms"}}  no_om_source   
+ 39959  {"event_content":{"apartment_link":"/apartment...       adwords   
+ 41847  {"page":{"page_type":"apartment_view","country...       adwords   
+ 71446  {"event_content":{"ga":{"category":"Filter","a...  no_om_source   
+ 
+       device_class         env  
+ 3058           bot  production  
+ 3068         other  production  
+ 39959       tablet  production  
+ 41847      desktop  production  
+ 71446        phone  production  >,
+ 'analysis_3': <bound method NDFrame.to_json of                                              0  \
+ 0     000727d438a0e2c05f6572373e74ee71c79d248b   
+ 1     001277072b953984f5d7fcf7575e06841fd30bc2   
+ 2     00442841b7b396c3358671e146a764602443d60f   
+ 3     00464d1e43c3de4bff653fcb75befb6f0a47ad9b   
+ 4     00484e342b45b0c1195ce9d48efc0bc3863e62b0   
+ ...                                        ...   
+ 3628  ffa92432b98ac27d446bef79f9f461a4209320f8   
+ 3629  ffaa802395d83ef7c3b4dc5b65fa7e7cf67344f5   
+ 3630  ffd3b0c4189d2bb7c1aecd73d0d924f230a3de83   
+ 3631  ffe7deadae038a7b488a00ee5ff33fd64276a60a   
+ 3632  fff8f0ca77cb0889d2701ff1e1c100f2572840c7   
+ 
+                                                       1  
+ 0                                  datetime  \
+ 3405 ...  
+ 1                                   datetime  \
+ 4668...  
+ 2                                   datetime  \
+ 3643...  
+ 3                                   datetime  \
+ 7460...  
+ 4                                 datetime  \
+ 680 20...  
+ ...                                                 ...  
+ 3628                               datetime  \
+ 5618 ...  
+ 3629                                datetime  \
+ 3807...  
+ 3630                               datetime  \
+ 2761 ...  
+ 3631                                datetime  \
+ 2456...  
+ 3632                                datetime  \
+ 4651...  
+ 
+ [3633 rows x 2 columns]>,
+ 'analysis_3-a': <bound method NDFrame.to_json of                                              0     1
+ 0     001277072b953984f5d7fcf7575e06841fd30bc2  True
+ 1     00442841b7b396c3358671e146a764602443d60f  True
+ 2     0048635b64173131a8eface0669e19406a8e4fdb  True
+ 3     004da9a8ea34b132bb8cb2f78899c74ced50a940  True
+ 4     005bd232b11eb192a669a68f27bb1408e87dff7e  True
+ ...                                        ...   ...
+ 1578  ff6e89de64b04de74bc89ff1cbe027cfc85113b6  True
+ 1579  ff75f462b1d9162807382e04e0bfd88a36f21605  True
+ 1580  ff7fdc82d386167be8b3ae5dc3cf4baf9605f05f  True
+ 1581  ffaa802395d83ef7c3b4dc5b65fa7e7cf67344f5  True
+ 1582  fff8f0ca77cb0889d2701ff1e1c100f2572840c7  True
+ 
+ [1583 rows x 2 columns]>,
+ 'analysis_3-b': <bound method NDFrame.to_json of                                              0     1
+ 0     00464d1e43c3de4bff653fcb75befb6f0a47ad9b  True
+ 1     0078698144856265558471927077e661e2834572  True
+ 2     00b97acbd4f9ca54c46a5315f784274c20cc4af2  True
+ 3     0119d163607911b5a889280e600775e1bb1f9c6c  True
+ 4     0145bf6d174173fa66688fa767b57bb66c87783d  True
+ ...                                        ...   ...
+ 1710  ff9c085ffd1ca785e255bc3bd94653d17f77af75  True
+ 1711  ffa92432b98ac27d446bef79f9f461a4209320f8  True
+ 1712  ffd3b0c4189d2bb7c1aecd73d0d924f230a3de83  True
+ 1713  ffe7deadae038a7b488a00ee5ff33fd64276a60a  True
+ 1714  fff8f0ca77cb0889d2701ff1e1c100f2572840c7  True
+ 
+ [1715 rows x 2 columns]>,
+ 'analysis_4': <bound method NDFrame.to_json of                                             0     1
+ 0    01ca56b32b60f2b0eacf791f76ace1febeb1da5f  True
+ 1    03a390caa232c2bbaea2e86247a5d242b53f282a  True
+ 2    044463626dee5259a26a894cd6d3546d97bf6d4b  True
+ 3    044d4bc6e7bab364c5b9001e5b96320bd812ca6d  True
+ 4    07be75ec9a40c59b424a44bc39b910b833da70e8  True
+ ..                                        ...   ...
+ 213  fdb5c350fd323bc298b86bef9a0811be322c914d  True
+ 214  feaa1981bfbed33c5cbe4b729e0985e3e4858bd5  True
+ 215  ff6e89de64b04de74bc89ff1cbe027cfc85113b6  True
+ 216  ff75f462b1d9162807382e04e0bfd88a36f21605  True
+ 217  fff8f0ca77cb0889d2701ff1e1c100f2572840c7  True
+ 
+ [218 rows x 2 columns]>,
+ 'analysis_5':                                            0     1
+ 0   02ab1821160b5cdaf85864425b332693fd5b30d3  True
+ 1   1181860330e7dde2f5311055c2c35cf08461ce20  True
+ 2   1a94327b6b004d3bfad282abb47900103c609e81  True
+ 3   1cb8087b1ab1e1cbc2b3c4be5a340832cd24544d  True
+ 4   21b1eeef4b7185dca14cfbb4a1c7dd9346c4dcc1  True
+ ..                                       ...   ...
+ 62  e75423678721928018eb24abebb2b3cda621aa17  True
+ 63  ed12c708c65e49b3b268a3b84369471b6f1bee2e  True
+ 64  f313207230e732d99b6fd8ee364a2f3425a374df  True
+ 65  f7e01bdc1a05d502a3a6c120ed45f376e68eb854  True
+ 66  feaa1981bfbed33c5cbe4b729e0985e3e4858bd5  True
+ 
+ [67 rows x 2 columns],
+ 'analysis_6': Empty DataFrame
+ Columns: []
+ Index: []}
